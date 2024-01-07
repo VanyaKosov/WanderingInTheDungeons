3749b97678ce48414e257039a0a5dd05052bda65
Ivan Kosov <kosov.vanya@gmail.com>
2024-01-07 12:15:53 -0600

    added category sorting to Inventory

--- Inventory.java
+++ Inventory.java
@@ -1,32 +1,44 @@
 import java.util.*;
 
 public class Inventory {
-    private final ArrayList<InventoryItem> inventory = new ArrayList<>();
+    public final Category<ItemWeapon> weaponItems = new Category<>();
+    public final Category<ItemConsumable> consumableItems = new Category<>();
+    public final Category<Item> otherItems = new Category<>();
 
     public void addItem(Item item, int amount, Player player) {
         if (!(item instanceof ItemConsumable)) {
             item.apply(player);
         }
 
-        for (InventoryItem inventoryItem : inventory) {
-            if (item == inventoryItem.item) {
-                inventoryItem.amount += amount;
-
-                return;
-            }
+        if (item instanceof ItemWeapon weapon) {
+            weaponItems.addItem(weapon, amount);
+        } else if (item instanceof ItemConsumable consumable) {
+            consumableItems.addItem(consumable, amount);
+        } else {
+            otherItems.addItem(item, amount);
         }
-        inventory.add(new InventoryItem(item, amount));
     }
 
-    public ArrayList<InventoryItem> getInventory() {
-        return inventory;
+    public static class Category<T extends Item> {
+        public final ArrayList<InventoryItem<T>> items = new ArrayList<>();
+
+        private void addItem(T item, int amount) {
+            for (InventoryItem<T> inventoryItem : items) {
+                if (item == inventoryItem.item) {
+                    inventoryItem.amount += amount;
+
+                    return;
+                }
+            }
+            items.add(new InventoryItem<T>(item, amount));
+        }
     }
 
-    public static class InventoryItem {
-        public final Item item;
+    public static class InventoryItem<T extends Item> {
+        public final T item;
         private int amount;
 
-        private InventoryItem(Item item, int amount) {
+        private InventoryItem(T item, int amount) {
             this.item = item;
             this.amount = amount;
         }
